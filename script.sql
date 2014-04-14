be83e27c2ccfaf2fa5c9d06619fd1f4db6a71ac5
Pape Ousmane Sow <papsow@emi.u-bordeaux1.fr>
2014-04-14 21:25:31 +0200

    modify script.sql

--- script.sql
+++ script.sql
@@ -1,9 +1,11 @@
-source ~/bd/transport.sql
-source ~/bd/client_csv_convert.sql
-source ~/bd/station_csv.sql
-source ~/bd/ligne.sql
-source ~/bd/station_ligne.csv.sql
-source ~/bd/carte.sql
-source ~/bd/tram.sql
+source ~/bd/Projet_bd/transport.sql
+source ~/bd/Projet_bd/client_csv_convert.sql
+source ~/bd/Projet_bd/station_csv.sql
+source ~/bd/Projet_bd/ligne.sql
+source ~/bd/Projet_bd/station_ligne.csv.sql
+source ~/bd/Projet_bd/carte.sql
+source ~/bd/Projet_bd/tram.sql
+source ~/bd/Projet_bd/chauffeur.sql
+source ~/bd/Projet_bd/chauf_tram.sql
 
 
