057331090b30d299b4787b643fd3efa29bff8db6
nureyni <nureyni@nureyni-SATELLITE-C50-A-1G1.(none)>
2014-04-15 09:58:46 +0200

    chauffeur

---
+++ chauffeur.sql
@@ -0,0 +1,12 @@
+
+INSERT INTO Chauffeur(nom_chauf) VALUES ('Hugo Nioteau');
+INSERT INTO Chauffeur(nom_chauf) VALUES ('Frank Anaba');
+INSERT INTO Chauffeur(nom_chauf) VALUES ('Jue Dong');
+INSERT INTO Chauffeur(nom_chauf) VALUES ('Ayoub Dra');
+INSERT INTO Chauffeur(nom_chauf) VALUES ('Hamza Dra');
+INSERT INTO Chauffeur(nom_chauf) VALUES ('Ousmane SOW');
+INSERT INTO Chauffeur(nom_chauf) VALUES ('Maxime Vincent');
+INSERT INTO Chauffeur(nom_chauf) VALUES ('Pape Birama Mbodj');
+INSERT INTO Chauffeur(nom_chauf) VALUES ('Ibrahima Toure');
+INSERT INTO Chauffeur(nom_chauf) VALUES ('oumar lo');
+INSERT INTO Chauffeur(nom_chauf) VALUES ('Baaba Yaar');
